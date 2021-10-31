387c3602df6e0eb387d8e2d5333d97276b2d6a3c
alidauda <alidauda14@gmail.com>
2021-10-31 22:07:16 +0100

    done

--- components/bottonca.js
+++ components/bottonca.js
@@ -5,7 +5,7 @@ import * as React from 'react';
  export default function Botton({AddItems}){
   
     return(
-<nav class="nav-1">
+<nav className="nav-1">
 
 <Button variant="contained" disableElevation  onClick={AddItems}>
     button
