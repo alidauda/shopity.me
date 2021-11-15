6e1b4d549f86a3b36c6be0a1fc0c211cef003b50
alidauda <alidauda14@gmail.com>
2021-11-15 18:47:57 +0100

    again

--- components/BottomNav.js
+++ components/BottomNav.js
@@ -4,7 +4,7 @@ export default function BottomNav({user,items}){
     console.log(user.username);
     return(
 
-      <nav className="nav-2 ">
+      <nav className="nav-2 "> 
 
     <Link href={`/${user.username}`} ><a className="   nav__link nav__link--active"><i className="material-icons nav__icon">home</i><span className="nav__text">home</span></a></Link>
    
