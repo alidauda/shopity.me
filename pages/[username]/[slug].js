9f98726e5363b73ae99da97f89e9af37bc84fc7e
alidauda <alidauda14@gmail.com>
2021-11-22 15:33:14 +0100

    done

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -29,7 +29,7 @@ export async function getStaticPaths(){
   const paths=snapshot.docs.map((doc)=>{
     const {slug, username}=doc.data();
     return{
-      params:{username,slug},
+      params:{username,slug}
 
     };
   });
