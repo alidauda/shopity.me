5ef1da4c747e00069587c5b91b3cf20cf694cf4b
alidauda <alidauda14@gmail.com>
2021-11-29 00:09:14 +0100

    done

--- pages/index.js
+++ pages/index.js
@@ -30,27 +30,10 @@ export default function Home() {
         <ul>
           <li><a className="nav-link scrollto active" href="#hero">Home</a></li>
           <li><a className="nav-link scrollto" href="#about">About</a></li>
-          <li><a className="nav-link scrollto" href="#services">Services</a></li>
-          <li><a className="nav-link scrollto" href="#portfolio">Portfolio</a></li>
-          <li><a className="nav-link scrollto" href="#team">Team</a></li>
-          <li><a href="blog.html">Blog</a></li>
-          <li className="dropdown"><a href="#"><span>Drop Down</span> <i className="bi bi-chevron-down"></i></a>
-            <ul>
-              <li><a href="#">Drop Down 1</a></li>
-              <li className="dropdown"><a href="#"><span>Deep Drop Down</span> <i className="bi bi-chevron-right"></i></a>
-                <ul>
-                  <li><a href="#">Deep Drop Down 1</a></li>
-                  <li><a href="#">Deep Drop Down 2</a></li>
-                  <li><a href="#">Deep Drop Down 3</a></li>
-                  <li><a href="#">Deep Drop Down 4</a></li>
-                  <li><a href="#">Deep Drop Down 5</a></li>
-                </ul>
-              </li>
-              <li><a href="#">Drop Down 2</a></li>
-              <li><a href="#">Drop Down 3</a></li>
-              <li><a href="#">Drop Down 4</a></li>
-            </ul>
-          </li>
+          <li><a className="nav-link scrollto" href="/privacy">Privacy Policy</a></li>
+          <li><a className="nav-link scrollto" href="/tem">Terms of Service</a></li>
+          
+          
           <li><a className="nav-link scrollto" href="#contact">Contact</a></li>
           <li><a className="getstarted scrollto" href="#about">Get Started</a></li>
         </ul>
@@ -64,14 +47,11 @@ export default function Home() {
     <div className="container">
       <div className="row">
         <div className="col-lg-6 d-flex flex-column justify-content-center">
-          <h1 data-aos="fade-up">We offer modern solutions for growing your business</h1>
-          <h2 data-aos="fade-up" data-aos-delay="400">We are team of talented designers making websites with Bootstrap</h2>
+          <h1 data-aos="fade-up">We allow small business to  create websites for free.</h1>
+          <h2 data-aos="fade-up" data-aos-delay="400"></h2>
           <div data-aos="fade-up" data-aos-delay="600">
             <div className="text-center text-lg-start">
-              <a href="#about" className="btn-get-started scrollto d-inline-flex align-items-center justify-content-center align-self-center">
-                <span>Get Started</span>
-                <i className="bi bi-arrow-right"></i>
-              </a>
+             
             </div>
           </div>
         </div>
@@ -92,17 +72,10 @@ export default function Home() {
 
           <div className="col-lg-6 d-flex flex-column justify-content-center" data-aos="fade-up" data-aos-delay="200">
             <div className="content">
-              <h3>Who We Are</h3>
-              <h2>Expedita voluptas omnis cupiditate totam eveniet nobis sint iste. Dolores est repellat corrupti reprehenderit.</h2>
-              <p>
-                Quisquam vel ut sint cum eos hic dolores aperiam. Sed deserunt et. Inventore et et dolor consequatur itaque ut voluptate sed et. Magnam nam ipsum tenetur suscipit voluptatum nam et est corrupti.
-              </p>
-              <div className="text-center text-lg-start">
-                <a href="#" className="btn-read-more d-inline-flex align-items-center justify-content-center align-self-center">
-                  <span>Read More</span>
-                  <i className="bi bi-arrow-right"></i>
-                </a>
-              </div>
+              <h3>What We do</h3>
+              <h2>we allow people to  create their website within 30 seconds  </h2>
+             
+             
             </div>
           </div>
 
@@ -116,240 +89,10 @@ export default function Home() {
     </section>
 
     
-    <section id="values" className="values">
-
-      <div className="container" data-aos="fade-up">
-
-        <header className="section-header">
-          <h2>Our Values</h2>
-          <p>Odit est perspiciatis laborum et dicta</p>
-        </header>
-
-        <div className="row">
-
-          <div className="col-lg-4">
-            <div className="box" data-aos="fade-up" data-aos-delay="200">
-              <img src="values-1.png" className="img-fluid" alt=""/>
-              <h3>Ad cupiditate sed est odio</h3>
-              <p>Eum ad dolor et. Autem aut fugiat debitis voluptatem consequuntur sit. Et veritatis id.</p>
-            </div>
-          </div>
-
-          <div className="col-lg-4 mt-4 mt-lg-0">
-            <div className="box" data-aos="fade-up" data-aos-delay="400">
-              <img src="assets/img/values-2.png" className="img-fluid" alt=""/>
-              <h3>Voluptatem voluptatum alias</h3>
-              <p>Repudiandae amet nihil natus in distinctio suscipit id. Doloremque ducimus ea sit non.</p>
-            </div>
-          </div>
-
-          <div className="col-lg-4 mt-4 mt-lg-0">
-            <div className="box" data-aos="fade-up" data-aos-delay="600">
-              <img src="values-3.png" className="img-fluid" alt=""/>
-              <h3>Fugit cupiditate alias nobis.</h3>
-              <p>Quam rem vitae est autem molestias explicabo debitis sint. Vero aliquid quidem commodi.</p>
-            </div>
-          </div>
-
-        </div>
-
-      </div>
-
-    </section>
+    
 
   
-    <section id="features" className="features">
-
-      <div className="container" data-aos="fade-up">
-
-        <header className="section-header">
-          <h2>Features</h2>
-          <p>Laboriosam et omnis fuga quis dolor direda fara</p>
-        </header>
-
-        <div className="row">
-
-          <div className="col-lg-6">
-            <img src="features.png" className="img-fluid" alt=""/>
-          </div>
-
-          <div className="col-lg-6 mt-5 mt-lg-0 d-flex">
-            <div className="row align-self-center gy-4">
-
-              <div className="col-md-6" data-aos="zoom-out" data-aos-delay="200">
-                <div className="feature-box d-flex align-items-center">
-                  <i className="bi bi-check"></i>
-                  <h3>Eos aspernatur rem</h3>
-                </div>
-              </div>
-
-              <div className="col-md-6" data-aos="zoom-out" data-aos-delay="300">
-                <div className="feature-box d-flex align-items-center">
-                  <i className="bi bi-check"></i>
-                  <h3>Facilis neque ipsa</h3>
-                </div>
-              </div>
-
-              <div className="col-md-6" data-aos="zoom-out" data-aos-delay="400">
-                <div className="feature-box d-flex align-items-center">
-                  <i className="bi bi-check"></i>
-                  <h3>Volup amet voluptas</h3>
-                </div>
-              </div>
-
-              <div className="col-md-6" data-aos="zoom-out" data-aos-delay="500">
-                <div className="feature-box d-flex align-items-center">
-                  <i className="bi bi-check"></i>
-                  <h3>Rerum omnis sint</h3>
-                </div>
-              </div>
-
-              <div className="col-md-6" data-aos="zoom-out" data-aos-delay="600">
-                <div className="feature-box d-flex align-items-center">
-                  <i className="bi bi-check"></i>
-                  <h3>Alias possimus</h3>
-                </div>
-              </div>
-
-              <div className="col-md-6" data-aos="zoom-out" data-aos-delay="700">
-                <div className="feature-box d-flex align-items-center">
-                  <i className="bi bi-check"></i>
-                  <h3>Repellendus mollitia</h3>
-                </div>
-              </div>
-
-            </div>
-          </div>
-
-        </div> 
-        <div className="row feature-icons" data-aos="fade-up">
-          <h3>Ratione mollitia eos ab laudantium rerum beatae quo</h3>
-
-          <div className="row">
-
-            <div className="col-xl-4 text-center" data-aos="fade-right" data-aos-delay="100">
-              <img src="features-3.png" className="img-fluid p-4" alt=""/>
-            </div>
-
-            <div className="col-xl-8 d-flex content">
-              <div className="row align-self-center gy-4">
-
-                <div className="col-md-6 icon-box" data-aos="fade-up">
-                  <i className="ri-line-chart-line"></i>
-                  <div>
-                    <h4>Corporis voluptates sit</h4>
-                    <p>Consequuntur sunt aut quasi enim aliquam quae harum pariatur laboris nisi ut aliquip</p>
-                  </div>
-                </div>
-
-                <div className="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
-                  <i className="ri-stack-line"></i>
-                  <div>
-                    <h4>Ullamco laboris nisi</h4>
-                    <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt</p>
-                  </div>
-                </div>
-
-                <div className="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
-                  <i className="ri-brush-4-line"></i>
-                  <div>
-                    <h4>Labore consequatur</h4>
-                    <p>Aut suscipit aut cum nemo deleniti aut omnis. Doloribus ut maiores omnis facere</p>
-                  </div>
-                </div>
-
-                <div className="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="300">
-                  <i className="ri-magic-line"></i>
-                  <div>
-                    <h4>Beatae veritatis</h4>
-                    <p>Expedita veritatis consequuntur nihil tempore laudantium vitae denat pacta</p>
-                  </div>
-                </div>
-
-                <div className="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="400">
-                  <i className="ri-command-line"></i>
-                  <div>
-                    <h4>Molestiae dolor</h4>
-                    <p>Et fuga et deserunt et enim. Dolorem architecto ratione tensa raptor marte</p>
-                  </div>
-                </div>
-
-                <div className="col-md-6 icon-box" data-aos="fade-up" data-aos-delay="500">
-                  <i className="ri-radar-line"></i>
-                  <div>
-                    <h4>Explicabo consectetur</h4>
-                    <p>Est autem dicta beatae suscipit. Sint veritatis et sit quasi ab aut inventore</p>
-                  </div>
-                </div>
-
-              </div>
-            </div>
-
-          </div>
-
-        </div>
-
-      </div>
-
-    </section>
-    <section id="team" className="team">
-
-      <div className="container" data-aos="fade-up">
-
-        <header className="section-header">
-          <h2>Team</h2>
-          <p>Our hard working team</p>
-        </header>
-
-        <div className="row gy-4">
-
-          <div className="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="100">
-            <div className="member">
-              <div className="member-img">
-                <img src="/team-1.jpg" className="img-fluid" alt=""/>
-                <div className="social">
-                  <a href=""><i className="bi bi-twitter"></i></a>
-                  <a href=""><i className="bi bi-facebook"></i></a>
-                  <a href=""><i className="bi bi-instagram"></i></a>
-                  <a href=""><i className="bi bi-linkedin"></i></a>
-                </div>
-              </div>
-              <div className="member-info">
-                <h4>Walter White</h4>
-                <span>Chief Executive Officer</span>
-                <p>Velit aut quia fugit et et. Dolorum ea voluptate vel tempore tenetur ipsa quae aut. Ipsum exercitationem iure minima enim corporis et voluptate.</p>
-              </div>
-            </div>
-          </div>
-
-         
-
-          <div className="col-lg-3 col-md-6 d-flex align-items-stretch" data-aos="fade-up" data-aos-delay="300">
-            <div className="member">
-              <div className="member-img">
-                <img src="/team-3.jpg" className="img-fluid" alt=""/>
-                <div className="social">
-                  <a href=""><i className="bi bi-twitter"></i></a>
-                  <a href=""><i className="bi bi-facebook"></i></a>
-                  <a href=""><i className="bi bi-instagram"></i></a>
-                  <a href=""><i className="bi bi-linkedin"></i></a>
-                </div>
-              </div>
-              <div className="member-info">
-                <h4>William Anderson</h4>
-                <span>CTO</span>
-                <p>Vero omnis enim consequatur. Voluptas consectetur unde qui molestiae deserunt. Voluptates enim aut architecto porro aspernatur molestiae modi.</p>
-              </div>
-            </div>
-          </div>
-
-          
-
-        </div>
-
-      </div>
-
-    </section>
+    
 
    
     <section id="contact" className="contact">
@@ -370,14 +113,14 @@ export default function Home() {
                 <div className="info-box">
                   <i className="bi bi-geo-alt"></i>
                   <h3>Address</h3>
-                  <p>A108 Adam Street,<br/>New York, NY 535022</p>
+                  <p><br/>No 10 Sam Okawraji gwarinpa Abuja</p>
                 </div>
               </div>
               <div className="col-md-6">
                 <div className="info-box">
                   <i className="bi bi-telephone"></i>
                   <h3>Call Us</h3>
-                  <p>+1 5589 55488 55<br/>+1 6678 254445 41
+                  <p>09060390088<br/>
                   </p>
                 </div>
               </div>
@@ -385,7 +128,7 @@ export default function Home() {
                 <div className="info-box">
                   <i className="bi bi-envelope"></i>
                   <h3>Email Us</h3>
-                  <p>info@example.com<br/>contact@example.com</p>
+                  <p>support@shopity.me<br/></p>
                 </div>
               </div>
               <div className="col-md-6">
@@ -420,9 +163,9 @@ export default function Home() {
           <div className="col-lg-5 col-md-12 footer-info">
             <a href="index.html" className="logo d-flex align-items-center">
               <img src="/logo.png" alt=""/>
-              <span>FlexStart</span>
+              <span>Shopity</span>
             </a>
-            <p>Cras fermentum odio eu feugiat lide par naso tierra. Justo eget nada terra videa magna derita valies darta donna mare fermentum iaculis eu non diam phasellus.</p>
+           
             <div className="social-links mt-3">
               <a href="#" className="twitter"><i className="bi bi-twitter"></i></a>
               <a href="#" className="facebook"><i className="bi bi-facebook"></i></a>
@@ -438,11 +181,9 @@ export default function Home() {
           <div className="col-lg-3 col-md-12 footer-contact text-center text-md-start">
             <h4>Contact Us</h4>
             <p>
-              A108 Adam Street <br/>
-              New York, NY 535022<br/>
-              United States <br/><br/>
-              <strong>Phone:</strong> +1 5589 55488 55<br/>
-              <strong>Email:</strong> info@example.com<br/>
+            No 10 Sam Okawraji gwarinpa Abuja <br/><br/>
+              <strong>Phone:</strong> 09060390088<br/>
+              <strong>Email:</strong> support@shopity.me<br/>
             </p>
 
           </div>
@@ -453,7 +194,7 @@ export default function Home() {
 
     <div className="container">
       <div className="copyright">
-        &copy; Copyright <strong><span>FlexStart</span></strong>. All Rights Reserved
+        &copy; Copyright <strong><span>Shopity</span></strong>. All Rights Reserved
       </div>
       <div className="credits">
         <a href="https://bootstrapmade.com/">BootstrapMade</a>
